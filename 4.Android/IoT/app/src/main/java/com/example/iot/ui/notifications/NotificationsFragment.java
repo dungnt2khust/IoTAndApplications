a3c9a2d5c7480fe7867bcf2dcd2c47701adabb8a
haiduongtd2k@gmail.com <duong.ndh183724@sis.hust.edu.vn>
2021-12-12 21:30:37 +0700

    add
    hd add

---
+++ 4.Android/IoT/app/src/main/java/com/example/iot/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,87 @@
+package com.example.iot.ui.notifications;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.iot.R;
+import com.example.iot.databinding.FragmentNotificationsBinding;
+import com.example.iot.network.HttpsTrustManager;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import okhttp3.OkHttpClient;
+
+public class NotificationsFragment extends Fragment {
+
+    private NotificationsViewModel notificationsViewModel;
+    private FragmentNotificationsBinding binding;
+    private Button loginBtn;
+    private EditText mUsername, mPassword;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        notificationsViewModel =
+                new ViewModelProvider(this).get(NotificationsViewModel.class);
+
+        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        loginBtn = binding.loginButton;
+        mUsername = binding.username;
+        mPassword = binding.password;
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+                HttpsTrustManager.allowAllSSL();
+                String url = "https://iotandapp.eddieonthecode.xyz/api/v1/User";
+                RequestQueue queue = Volley.newRequestQueue(getContext());
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.d("MSG", response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("ERROR", error.toString());
+                    }
+                });
+
+                queue.add(stringRequest);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
