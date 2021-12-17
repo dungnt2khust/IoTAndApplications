8ef5eca5f55985a6691e9acf67386dbe9a89c15c
haiduongtd2k@gmail.com <duong.ndh183724@sis.hust.edu.vn>
2021-12-17 21:34:01 +0700

    add
    add

--- 4.Android/IoT/app/src/main/java/com/example/iot/ui/home/HomeFragment.java
+++ 4.Android/IoT/app/src/main/java/com/example/iot/ui/home/HomeFragment.java
@@ -1,14 +1,25 @@
 package com.example.iot.ui.home;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,13 +29,46 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.iot.R;
 import com.example.iot.databinding.FragmentHomeBinding;
+import com.example.iot.device.DeviceItem;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class HomeFragment extends Fragment {
 
+    private static final int REQUEST_BLUETOOTH = 1;
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
     private ListView listView;
-    private Button scanDeviceBtn;
+    private ToggleButton scanDeviceBtn;
+    BluetoothAdapter bTAdapter;
+    private Set<BluetoothDevice> pairedDevices;
+    ArrayList<DeviceItem>listDevices;
+    private ArrayAdapter<DeviceItem> mAdapter;
+
+    private final BroadcastReceiver bReciever = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Log.d("DEVICELIST", "Bluetooth device found\n");
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Create a new device item
+                DeviceItem newDevice = new DeviceItem(device.getName(), device.getAddress(), "false");
+                // Add it to our adapter
+                boolean isContain = false;
+                for(DeviceItem dv : listDevices){
+                    if(dv.getAddress().equals(device.getAddress())){
+                        isContain = true;
+                        break;
+                    }
+                }
+                if(!isContain){
+                    mAdapter.add(newDevice);
+                    mAdapter.notifyDataSetChanged();
+                }
+            }
+        }
+    };
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
@@ -34,9 +78,35 @@ public class HomeFragment extends Fragment {
         View root = binding.getRoot();
         listView = binding.listItem;
         scanDeviceBtn = binding.Search;
-        String value[]={"asda","Ansar","Nisam"};
-        ArrayAdapter<String>adapter=new ArrayAdapter<String>(getContext(),R.layout.device_result,R.id.textView, value);
-        listView.setAdapter(adapter);
+        bTAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (!bTAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_BLUETOOTH);
+        }
+        listDevices = new ArrayList<DeviceItem>();
+        mAdapter = new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1, listDevices);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("DEVICELIST", "onItemClick position: " + position +
+                        " id: " + id + " name: " + listDevices.get(position).getDeviceName() + " address: "+ listDevices.get(position).getAddress()+ "\n"  );
+            }
+        });
+        scanDeviceBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                if(isChecked){
+                    scanDevice();
+                    getActivity().registerReceiver(bReciever, filter);
+                    bTAdapter.startDiscovery();
+                }
+                else{
+                    getActivity().unregisterReceiver(bReciever);
+                    bTAdapter.cancelDiscovery();
+                }
+            }
+        });
         return root;
     }
 
@@ -45,4 +115,18 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    void scanDevice(){
+        pairedDevices = bTAdapter.getBondedDevices();
+        for(BluetoothDevice device : pairedDevices){
+            DeviceItem newDevice= new DeviceItem(device.getName(),device.getAddress(),"false");
+            listDevices.add(newDevice);
+        }
+        Toast.makeText(getContext(),"Showing Paired Devices",Toast.LENGTH_SHORT).show();
+        listView.setAdapter(mAdapter);
+    }
+    void stopScan(){
+    }
+
+
 }
